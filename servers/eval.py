8f9d944bddc113e7e47c814b4cd22c18aa559341
Aleksey Bilogur <aleksey@residentmar.io>
2020-11-19 15:23:27 -0500

    Add basic eval.

---
+++ servers/eval.py
@@ -0,0 +1,191 @@
+"""
+MobileNet model with quantization-aware training (QAT) enabled. Trained on CIFAR10.
+
+This file batches training and evaluation into the same script. Since QAT requires careful
+management of the training loop, it's easiest do both in the same run.
+"""
+# Forked from https://github.com/tonylins/pytorch-mobilenet-v2/blob/master/MobileNetV2.py
+
+# TODO: can we train on CUDA, then quantize on CPU? This bears investigating.
+
+####################
+# MODEL DEFINITION #
+####################
+
+import torch.nn as nn
+import math
+import torch
+import os
+
+
+def conv_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+def conv_1x1_bn(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+def make_divisible(x, divisible_by=8):
+    import numpy as np
+    return int(np.ceil(x * 1. / divisible_by) * divisible_by)
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(inp * expand_ratio)
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        if expand_ratio == 1:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, n_class=1000, input_size=224, width_mult=1.):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 1280
+        interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        # building first layer
+        assert input_size % 32 == 0
+        # input_channel = make_divisible(input_channel * width_mult)  # first channel is always 32!
+        self.last_channel = make_divisible(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        self.features = [conv_bn(3, input_channel, 2)]
+        # building inverted residual blocks
+        for t, c, n, s in interverted_residual_setting:
+            output_channel = make_divisible(c * width_mult) if t > 1 else c
+            for i in range(n):
+                if i == 0:
+                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                else:
+                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                input_channel = output_channel
+        # building last several layers
+        self.features.append(conv_1x1_bn(input_channel, self.last_channel))
+        # make it nn.Sequential
+        self.features = nn.Sequential(*self.features)
+
+        # building classifier
+        self.classifier = nn.Linear(self.last_channel, n_class)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.mean(3).mean(2)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+def get_model():
+    return MobileNetV2(width_mult=1, n_class=10, input_size=32)
+
+
+##############
+# EVALUATION #
+##############
+
+def get_dataloader():
+    import torchvision
+    from torch.utils.data import DataLoader
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomPerspective(),
+        torchvision.transforms.ToTensor()
+    ])
+    dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=True, transform=transform, download=True)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    return dataloader
+
+def eval_fn(model):
+    import numpy as np
+    if torch.cuda.device_count() >= 1:
+        device = torch.device('cuda')
+        is_cpu = False
+    else:
+        device = torch.device('cpu')
+        is_cpu = True
+
+    checkpoints_dir = '/spell/checkpoints'
+    if is_cpu:
+        model.load_state_dict(
+            torch.load(f"{checkpoints_dir}/model_10.pth", map_location=torch.device('cpu'))
+        )
+    else:
+        model.load_state_dict(torch.load(f"{checkpoints_dir}/model_10.pth"))        
+    model.eval()
+
+    print(f"Evaluating the model...")
+    start_time = time.time()
+    for i, (X_batch, y_cls) in enumerate(dataloader):
+        y = y_cls
+        y_pred = model(X_batch)
+    print(f"Evaluation done in {str(time.time() - start_time)} seconds.")
+
+if __name__ == "__main__":
+    dataloader = get_dataloader()
+    model = get_model()
+    eval_fn(model)
